b0ea48d0ed837086eaac68ed95d3474d6cf6cc59
juagiag1 <44643456+juagiag1@users.noreply.github.com>
2019-12-29 10:54:19 +0100

    initial point

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h1 style="text-align: center;">Datos recuperados de la BBDD</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Correo</th>
+      <th scope="col">Password</th>
+      <th scope="col">Nombre</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>prueba1@mail.com</td>
+      <td>123</td>
+      <td>Prueba1</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>prueba2@mail.com</td>
+      <td>123</td>
+      <td>Prueba2</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>prueba3@mail.com</td>
+      <td>123</td>
+      <td>Prueba3</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+</body>
+</html>
